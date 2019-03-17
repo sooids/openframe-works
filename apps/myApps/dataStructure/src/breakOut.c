764f14772264e1899d7ccdfdec3e4c48ce44d08b
soo <soo@ecdsa.io>
2019-03-18 02:22:51 +0900

    add bar

--- apps/myApps/dataStructure/src/breakOut.c
+++ apps/myApps/dataStructure/src/breakOut.c
@@ -7,6 +7,10 @@ void initGame() {
 	game.ballX = ofGetWidth() / 2;
 	game.ballY = ofGetHeight() / 2;
 	game.ballDirX = game.ballDirY = game.ballSpeed = 10;
+
+	game.padX = ofGetWidth() / 2;
+	game.padY = ofGetHeight() - 50;
+	game.padWidth = 400;
 }
 
 void moveBall() {
@@ -18,11 +22,28 @@ void moveBall() {
 
 void bounceBall() {
 	if (game.ballX < BALLRADIUS || game.ballX > ofGetWidth() - BALLRADIUS) game.ballDirX *= -1;
-	if (game.ballY < BALLRADIUS || game.ballY > ofGetHeight() - BALLRADIUS) game.ballDirY *= -1;
+	// if (game.ballY < BALLRADIUS || game.ballY > ofGetHeight() - BALLRADIUS) game.ballDirY *= -1;
+	if (game.ballY < BALLRADIUS) game.ballDirY *= -1;
+
+	if (game.ballY > ofGetHeight() - BALLRADIUS) game.ballDirY *= -1;
+	else if (game.ballY > game.padY - BALLRADIUS &&
+		game.ballX >= game.padX - game.padWidth / 2 &&
+		game.ballX <= game.padX + game.padWidth / 2)
+		game.ballDirY *= -1;
 }
 
 void drawBall() {
 	ofFill();
 	ofSetColor(ofColor::black);
 	ofDrawCircle(game.ballX, game.ballY, BALLRADIUS);
+}
+
+void movePad() {
+	game.padX = ofGetMouseX();
+}
+
+void drawPad() {
+	ofFill();
+	ofSetColor(ofColor::black);
+	ofDrawLine(game.padX - game.padWidth / 2, game.padY, game.padX + game.padWidth / 2, game.padY);
 }
--- apps/myApps/dataStructure/src/breakOut.h
+++ apps/myApps/dataStructure/src/breakOut.h
@@ -7,10 +7,14 @@ typedef struct {
 	int ballX, ballY;
 	int ballDirX, ballDirY;
 	int ballSpeed;
+	int padX, padY, padWidth;
 } Brick;
 
 void initGame();
 void moveBall();
 void drawBall();
 void bounceBall();
+
+void movePad();
+void drawPad();
 #endif
